040561c175a484d127595bbea650fb68f717f602
MilkFansHelloWorld <31027215+MilkFansHelloWorld@users.noreply.github.com>
2018-07-30 21:42:18 +0800

    nnn

---
+++ users.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
+--
+-- Host: localhost    Database: platformdb
+-- ------------------------------------------------------
+-- Server version	8.0.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8mb4 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(32) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `firstname` varchar(100) DEFAULT NULL,
+  `lastname` varchar(100) DEFAULT NULL,
+  `location` varchar(1000) DEFAULT NULL,
+  `creation_date` date DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `cert` varchar(5000) DEFAULT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'f63871aa012445b2b21c7ea9044567a4','user1',NULL,NULL,NULL,'2018-07-28',NULL,NULL),(2,'bf82ae97d5854248a6228fe604ba0dc2','user2',NULL,NULL,NULL,'2018-07-28',NULL,NULL),(3,'fd2036d511d942ddb3d79e85137de1d1','user3',NULL,NULL,NULL,'2018-07-28',NULL,'./client-ssl/3.p12'),(4,'ba5e652072a643538b695a149045f573','user4',NULL,NULL,NULL,'2018-07-28',NULL,'./client-ssl/4.p12'),(5,'42ae570fb37d4d3c8a752c38aed9eaf7','user5',NULL,NULL,NULL,'2018-07-28',NULL,'./client-ssl/5.p12'),(6,'d21a1a9a7b9a4f9a9a58881bf7819592','user6',NULL,NULL,NULL,'2018-07-28',NULL,'./client-ssl/6.p12'),(7,'afa807d093a211e8a3f3d3cb06675ba8','user7',NULL,NULL,NULL,'2018-07-30',NULL,'./client-ssl/7.p12'),(8,'699c7be093ca11e8a73d1dbbee9d9cb0','user8',NULL,NULL,NULL,'2018-07-30',NULL,'./client-ssl/8.p12'),(9,'7f2b04d093cb11e8be6b99965baa00c0','user9',NULL,NULL,NULL,'2018-07-30',NULL,'./client-ssl/9.p12'),(10,'6f00bfe093f911e88f74a7ac0ee63569','user10',NULL,NULL,NULL,'2018-07-30',NULL,'./client-ssl/10.p12');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-30 21:38:54
